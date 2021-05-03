e5689b7eeb24bd3c8c3fbf184ee2abf2dc5d8c99
dmellonikita09 <dmellonikita09@gmail.com>
2021-05-03 12:35:30 -0700

    sixth commit

--- flask_web/app/models.py
+++ flask_web/app/models.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
-
+    
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
@@ -26,6 +26,7 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    #upload_photo = db.Column(db.String(20), nullable=False, default='default.jpg')
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
--- flask_web/app/routes.py
+++ flask_web/app/routes.py
@@ -1,27 +1,18 @@
+
+import os
+import secrets
+#from PIL import Image
+
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-post = [
-    {
-        'item_name': 'Green Bottle',
-        'title': 'post 1',
-        'day_posted': 'saturday',
-        'date': '10th feb',
-        'content': 'Found a Green colored bottle '
-    },
-    {
-        'item_name': 'Yellow Umbrella',
-        'title': 'post 2',
-        'day_posted': 'sunday',
-        'date': '11th feb',
-        'content': 'Found a Yellow colored umbrella'
-    }
-]
+
 
 @app.route('/')
 def home():
+    post=Post.query.all()
     return render_template('home.html', post=post)
 
 @app.route('/about')
@@ -61,7 +52,44 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/picture', picture_fn) 
+    form_picture.save(picture_path)
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.pictures.data:
+            picture_file = save_picture(form.pictures.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email 
+    image_file = url_for('static', filename='picture/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    #upload_photo = url_for('static', filename='upload/' + current_user.upload_photo)
+    return render_template('create_post.html', title='New Post', form=form)
