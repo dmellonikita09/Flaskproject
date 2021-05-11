12373c66041a40f0f3b5ee231220bd1513f3c6c7
dmellonikita09 <dmellonikita09@gmail.com>
2021-05-11 16:19:55 -0700

    eighth commit

--- flask_web/app/__init__.py
+++ flask_web/app/__init__.py
@@ -1,7 +1,11 @@
+import os
+# import myEnvVal
+# myEnvVal.setVar()
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '97924718a878ea65af465d4656b51eb5'
@@ -12,5 +16,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')  
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD') 
+mail = Mail(app)
 
 from app import routes
